cfbf41a374e5adaf6d183369fc516bcfe6e506d0
Gavin Riggs <70348030+Gavinmr1@users.noreply.github.com>
2020-10-27 19:48:09 -0500

    error fix
    fixed merge error in code

--- script.js
+++ script.js
@@ -1,207 +1,133 @@
-<<<<<<< HEAD
-    var APIKey = 'e7610467436ab1c59773adeceb236ff7'
+var city=""; 
+var url="";
+var key="";
+var queryUrl ="";
+var currentUrl = "";
+var citiesDiv = document.getElementById("recentSearches");
+var cities = []; 
 
+function init(){
+    var savedCities = JSON.parse(localStorage.getItem("cities"));
+    if (savedCities !== null){
+        cities = savedCities
+    }   
+    renderButtons(); 
+}
+init();
 
+function clickList(){
+    $(".listbtn").on("click", function(event){
+        event.preventDefault();
+        city = $(this).text().trim();
+        APIcalls(); 
+    })
+}
+clickList();
 
-
-=======
-$(document).ready(function () {
-    
-    var APIKey = 'e7610467436ab1c59773adeceb236ff7';
-    populateSearch();
-    defaultSearch();
-
-<<<<<<< HEAD
-=======
-    
->>>>>>> cfccf1e493f3c9a6e77b560528e4abf2496b193b
-
-    
-    
->>>>>>> develop
-    function exchangeRates() {
-        var query = "http://api.currencylayer.com/live?access_key=c783200a0ae3d77071075137f56ccece";
-            $.ajax({
-              url: query,
-              method: "GET"
-
-            }).then(function (response) {
-              console.log(response);
-              $('.currency').text('Currency:' + ' USD/GBP ' + response.quotes.USDGBP + ' USD/EUR ' + response.quotes.USDEUR + ' USD/JPY ' + response.quotes.USDJPY + ' USD/RUB ' + response.quotes.USDRUB)
-            });
-      }
-
-
-    function hotelBooking(city) {
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + city,
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "hotels4.p.rapidapi.com",
-                "x-rapidapi-key": "efb9190d78msh44fd7735c9188a6p17edbfjsn0f3829e74837"
-            }
+function searchClicker() {
+    $("#searchbtn").on("click", function(event){
+        event.preventDefault();
+        city = $(this).prev().val().trim()
+        cities.push(city);
+        if(cities.length > 8){
+            cities.shift()
         }
-        
-        $.ajax(settings).done(function (response) {
-<<<<<<< HEAD
-/*feature-map */ 
-            console.log('City: ', response);
-            var coord = response.suggestions[0].entities[0];
-            console.log('Coord: ', coord)
-            initMap(coord.latitude,coord.longitude )
-        
-            console.log(response);
-/* develop */
-=======
-            console.log('City: ', response);
-            var coord = response.suggestions[0].entities[0];
-            console.log('Coord: ', coord)
-            $('#map').empty().append(`
-            
-            <iframe
-            class="iframe"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d24823.574025673028!2d-${coord.latitude}!3d${coord.longitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1603224169167!5m2!1sen!2sus"
-            width="400"
-            height="300"
-            frameborder="0"
-            style="border: 0"
-            allowfullscreen=""
-            aria-hidden="false"
-            tabindex="0"
-          ></iframe>
+        if (city == ""){
+            return; 
+        }
+        APIcalls();
+        cityStorage(); 
+        renderButtons();
+    })
+}
+searchClicker();
+ 
+function cityStorage(){
+    localStorage.setItem("cities", JSON.stringify(cities)); 
+}
 
-            `)
-
->>>>>>> develop
-            $('.hotels').text('Hotels: ' + response.suggestions[3].entities[0].name + '/' + response.suggestions[3].entities[1].name )
-        });
-            
-            
-        };
-    
-
-    function restaurants(city) {
-        const settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://rapidapi.p.rapidapi.com/photos",
-            "method": "POST",
-            "headers": {
-                "content-type": "application/x-www-form-urlencoded",
-                "x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com",
-                "x-rapidapi-key": "efb9190d78msh44fd7735c9188a6p17edbfjsn0f3829e74837"
-            },
-            "data": {
-                "language": "en_US",
-                "location_id": "15333482",
-                "currency": "USD",
-                "limit": "1"
-            }
-        };
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        });
+function renderButtons(){
+    citiesDiv.innerHTML = ""; 
+    if(cities == null){
+        return;
+    }
+    var newCity = [...new Set(cities)];
+    for(var i=0; i < newCity.length; i++){
+        var cityName = newCity[i]; 
+        var buttonEl = document.createElement("button");
+        buttonEl.textContent = cityName; 
+        buttonEl.setAttribute("class", "listbtn"); 
+        citiesDiv.appendChild(buttonEl);
+        clickList();
+      }
     }
 
-     function weatherCall(city) {
-        console.log(city)
-     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=' + APIKey + '&units=imperial';
-
-         $.ajax({
-             url: queryURL,
-             method: 'GET'
-            })
-
-             .then(function(response) {
-                 console.log(queryURL)
-                 console.log(response)
-                 $('.weather').text('Weather: ' + response.main.temp + ' °F')
-                 $('.location').text('Location: ' + response.name) 
-                
-            });
-        }
-
-
-        function populateSearch() {
-            var searchHistory = localStorage.getItem('searchHistory') || '[]';
-            var listOfSearchHistory = [...JSON.parse(searchHistory)];
-            document.getElementById("search-text").value = "";
-            document.getElementById("search-list").innerHTML = "";
-            for (var i = listOfSearchHistory.length - 1; i >= 0; i--) {
-              var listElement = document.createElement("li");
-              var clickableElement = document.createElement("a");
-              clickableElement.href = "#";
-              clickableElement.textContent = listOfSearchHistory[i];
-              document.getElementById("search-list").appendChild(listElement);
-              listElement.appendChild(clickableElement);
-              clickableElement.onclick = searchCity;
-            }
-          }
-
-
-          function searchCity() {
-            var search = this.textContent;
-            exchangeRates(search);
-            hotelBooking(search);
-            restaurants(search);
-            weatherCall(search);
-            var searchHistory = localStorage.getItem('searchHistory') || '[]';
-            var listOfSearchHistory = [...JSON.parse(searchHistory), search];
-            localStorage.setItem("searchHistory", JSON.stringify(listOfSearchHistory));
-            populateSearch();
-          }
-        
-     
-          function defaultSearch() {
-            var searchHistory = localStorage.getItem('searchHistory') || '[]';
-            var listOfSearchHistory = [...JSON.parse(searchHistory)];
-            search = listOfSearchHistory[listOfSearchHistory.length - 1];
-            exchangeRates(search);
-            hotelBooking(search);
-            restaurants(search);
-            weatherCall(search);
-          }
-
-
-    $('#search-form').on('submit', function(e){
-        e.preventDefault()
-        var search = $('#search-text').val()
-        exchangeRates(search);
-        hotelBooking(search);
-        restaurants(search);
-<<<<<<< HEAD
-        weatherCall(search)
-    })
-=======
-        weatherCall(search);
-        var searchHistory = localStorage.getItem('searchHistory') || '[]';
-        var listOfSearchHistory = [...JSON.parse(searchHistory), search];
-        localStorage.setItem("searchHistory", JSON.stringify(listOfSearchHistory));
-        populateSearch();
-    })
-
-<<<<<<< HEAD
-    /* feature-map */
-
+function APIcalls(){
+    url = "https://api.openweathermap.org/data/2.5/forecast?q=";    
+    currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+    key = "&appid=cd12c82f6f4c333cf972f80da362c556";
+    queryUrl = url + city + key;
+    currentWeatherUrl = currentUrl + city + key; 
     
+    $("#cityName").text("Today's Weather in " + city);
+    $.ajax({
+    url: queryUrl,
+    method: "GET",
+    }).then(function(response){
+        var dayNum = 0;
+        for(var i = 0; i < response.list.length; i++) {
+            if(response.list[i].dt_txt.split(" ")[1] == "15:00:00") {
+                var day = response.list[i].dt_txt.split("-")[2].split(" ")[0];
+                var month = response.list[i].dt_txt.split("-")[1];
+                var year = response.list[i].dt_txt.split("-")[0];
+                var temp = Math.round(((response.list[i].main.temp - 273.15) *9/5+32));
+                
+                $("#" + dayNum + "date").text(month + "/" + day + "/" + year); 
+                $("#" + dayNum + "forcastTemp").text("Temp: " + temp + String.fromCharCode(176)+"F");
+                $("#" + dayNum + "forcastHum").text("Humidity: " + response.list[i].main.humidity);
+                $("#" + dayNum + "forcastIcon").attr("src", "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png");
+                dayNum++; 
+            }   
+        }
+    });
 
-    function initMap(lat, lon) {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: lat, lng: lon },
-            scrollwheel: true,
-            zoom: 8
+    $.ajax({
+        url:currentWeatherUrl,
+        method: "GET", 
+    }).then(function(currentData){
+        var temp = Math.round(((currentData.main.temp - 273.15) * 9/5 + 32))
+        $("#currentTemp").text("Temperature: " + temp + String.fromCharCode(176)+"F");
+        $("#currentHum").text("Humidity: " + currentData.main.humidity);
+        $("#currentWind").text("Wind Speed: " + currentData.wind.speed);
+        $("#currentIcon").attr({"src": "http://openweathermap.org/img/w/" + currentData.weather[0].icon + ".png",
+            "height": "100px", "width":"100px"});
+        var lon = currentData.coord.lon;
+        var lat = currentData.coord.lat;
+
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?="+ key+ "&lat=" + lat +"&lon=" + lon;
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        }).then(function(responseuv) {
+            var cityUV = $("<span>").text(responseuv.value);
+            var cityUVp = $("#currentUv").text("UV Index: ");
+            cityUVp.append(cityUV);
+            $("#currentUv").append(cityUVp);
+            if(responseuv.value > 0 && responseuv.value <=2){
+                cityUV.attr("class","green")
+            }
+            else if (responseuv.value > 2 && responseuv.value <= 5){
+                cityUV.attr("class","yellow")
+            }
+            else if (responseuv.value >5 && responseuv.value <= 7){
+                cityUV.attr("class","orange")
+            }
+            else if (responseuv.value >7 && responseuv.value <= 10){
+                cityUV.attr("class","red")
+            }
+            else{
+                cityUV.attr("class","purple")
+            }
         });
-    } 
-
-
-
-});
-
-
-/* develop */
-=======
-});
->>>>>>> cfccf1e493f3c9a6e77b560528e4abf2496b193b
->>>>>>> develop
+     })
+}
